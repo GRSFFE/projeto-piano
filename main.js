4574523f4d49c668cff7b6db217c22b68cd101ad
Germanno Resende <germannoresende@gmail.com>
2023-01-11 19:28:02 -0300

    start a project

---
+++ main.js
@@ -0,0 +1,11 @@
+// get all keys
+const keys = document.querySelectorAll(".key")
+
+// play notes
+
+// click with mouse
+
+// keyboard type
+window.addEventListener("keydown", function(event){
+    console.log(event)
+})
